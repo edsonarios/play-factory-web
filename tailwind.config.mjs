be370a836a2fcafebb94e22015554fc4d7daa3b5
edsonarios <edsonrios9@gmail.com>
2024-05-31 19:20:38 -0400

    Added navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+        fblue: '#00b2ff'
+      },
+			textColor: {
+				fblue: '#00b2ff',
+			},
+			boxShadowColor: {
+				fblue: '#00b2ff',
+			},
+		},
 	},
 	plugins: [],
 }
