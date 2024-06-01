9824e20eaa2a689e3c5ddc233f03610fe52b64e3
edsonarios <edsonrios9@gmail.com>
2024-05-31 21:06:48 -0400

    Added main features and otherPlatforms animation

---
+++ src/utils/aos.script.ts
@@ -0,0 +1,10 @@
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+export function aosInit() {
+  AOS.init({
+    duration: 1000,
+    delay: 150,
+    once: true,
+  })
+}
---
+++ src/utils/constants.ts
@@ -0,0 +1,3 @@
+export const appName = 'Play Factory'
+export const urlMainWeb = 'https://edevv.dev/'
+export const urlWeb = 'https://play-factory.edevv.dev/'
