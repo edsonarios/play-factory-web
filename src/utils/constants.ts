fb6986ea35949c76cef952d79461d575bc8dd1e3
edsonarios <edsonrios9@gmail.com>
2024-06-01 17:41:08 -0400

    Added footer

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,11 @@
 export const appName = 'Play Factory'
 export const urlMainWeb = 'https://edevv.dev/'
-export const urlWeb = 'https://play-factory.edevv.dev/'
+export const urlLinkedin = 'https://www.linkedin.com/in/edson-a%C3%B1awaya/'
+export const urlGithub = 'https://github.com/edsonarios'
+
+export const urlPlayWeb = 'https://play.edevv.dev/'
+export const urlPlayFactoryWeb = 'https://play-factory.edevv.dev/'
+
+export const mainRepository = 'https://github.com/edsonarios/play-factory-dev'
+export const issuesRepository = 'https://github.com/edsonarios/play-factory-dev/issues'
+export const discussionsRepository = 'https://github.com/edsonarios/play-factory-dev/discussions'
