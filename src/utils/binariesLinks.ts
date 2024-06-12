2170e57f3c8a8145b5fea30242597672cdcd0feb
edsonarios <edsonrios9@gmail.com>
2024-06-11 21:44:28 -0400

    Update links to main version

--- src/utils/binariesLinks.ts
+++ src/utils/binariesLinks.ts
@@ -1,4 +1,4 @@
-const version = '0.0.9'
+const version = '1.0.0'
 export const windowsLink = `https://github.com/edsonarios/play-factory-dev/releases/download/${version}/Play-Factory-Windows-${version}.exe`
 export const macLink = `https://github.com/edsonarios/play-factory-dev/releases/download/${version}/Play-Factory-Mac-${version}-universal.dmg`
 export const linuxLink = `https://github.com/edsonarios/play-factory-dev/releases/download/${version}/Play-Factory-Linux-${version}.deb`
